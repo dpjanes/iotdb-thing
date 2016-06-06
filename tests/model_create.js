bd188e509341cb5c72177d55db9c9c655560e28c
David Janes <davidjanes@davidjanes.com>
2016-06-06 12:20:37 -0400

    test model band

---
+++ tests/model_create.js
@@ -0,0 +1,80 @@
+/*
+ *  model_create.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2016-06-06
+ *
+ *  Copyright [2013-2016] [David P. Janes]
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict";
+
+const fs = require("fs");
+
+const iotdb = require("iotdb");
+const _ = iotdb._;
+
+const assert = require("assert");
+const thing = require("../thing");
+
+describe("model", function() {
+    describe("create", function() {
+        it("default", function() {
+            const thing_1 = thing.make();
+            const model_1 = thing_1.band("model");
+
+            assert.ok(model_1);
+            assert.strictEqual(model_1.band_name(), "model");
+            assert.strictEqual(model_1.thing(), thing_1);
+        });
+        it("initialized", function() {
+            const thing_1 = thing.make({
+                model: JSON.parse(fs.readFileSync('./things/thing-basement-heater/model', 'utf-8')),
+            });
+            const model_1 = thing_1.band("model");
+
+            assert.ok(model_1);
+            assert.strictEqual(model_1.band_name(), "model");
+            assert.strictEqual(model_1.thing(), thing_1);
+
+            console.log(model_1.state());
+        });
+    });
+    describe("update", function() {
+        it("load from file", function() {
+            const thing_1 = thing.make();
+            const model_1 = thing_1.band("model");
+            model_1.update(JSON.parse(fs.readFileSync('./things/thing-basement-heater/model', 'utf-8')));
+
+            assert.ok(model_1);
+            assert.strictEqual(model_1.band_name(), "model");
+            assert.strictEqual(model_1.thing(), thing_1);
+
+            console.log(model_1.state());
+        });
+        it("load from file (expanded)", function() {
+            const thing_1 = thing.make();
+            const model_1 = thing_1.band("model");
+            model_1.update(_.ld.expand(JSON.parse(fs.readFileSync('./things/thing-basement-heater/model', 'utf-8'))));
+
+            assert.ok(model_1);
+            assert.strictEqual(model_1.band_name(), "model");
+            assert.strictEqual(model_1.thing(), thing_1);
+
+            console.log(model_1.state());
+        });
+    });
+});
