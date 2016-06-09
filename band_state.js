7a0c4ccb6fb20851d5e9252f328f5fc1f9322c5b
David Janes <davidjanes@davidjanes.com>
2016-06-09 15:57:18 -0230

    code cleanup

--- band_state.js
+++ band_state.js
@@ -43,40 +43,37 @@ const make = (_thing, _d, _band) => {
         const thing = self.thing();
 
         _.mapObject(ud, ( uvalue, ukey ) => {
-            const attribute = thing.attribute(ukey);
             const key = helpers.state_lookup_key(ukey, thing);
-            if (!attribute || !key) {
+            if (!key) {
                 rds.push({
                     key: ukey,
                     value: uvalue,
                     is_validated: false,
                 });
-                return;
+            } else {
+                rds.push({
+                    key: key,
+                    value: uvalue,
+                });
             }
-
-            const rd = _.d.compose.shallow({
-                key: key,
-                value: uvalue,
-            }, attribute);
-
-            rds.push(rd);
         });
 
         return rds;
     };
+
     self._prepare_set = ( ukey, uvalue, as_type ) => {
         const rds = [];
         const thing = self.thing();
 
-        const attribute = thing.attribute(ukey);
         const key = helpers.state_lookup_key(ukey, thing);
-        if (!attribute || !key) {
+        if (!key) {
             rds.push({
                 key: ukey,
                 value: uvalue,
                 is_validated: false,
             });
         } else if (as_type) {
+            const attribute = thing.attribute(ukey);
             const value = cast.cast(uvalue, as_type, attribute);
             if (_.is.Undefined(value)) {
                 rds.push({
